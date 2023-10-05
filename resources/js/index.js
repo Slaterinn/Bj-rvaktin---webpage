9da1dcc38cf59673d55bb4b45911dc061dce24e6
Þröstur Pétursson <Throstur.Petursson@landsbankinn.is>
2023-10-05 09:39:44 +0000

    Update index.js

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,17 +1,15 @@
 $(document).ready(function () {
     // Get all div elements with the class .scroll-target
-    var $divs = $('.scroll-target');
-    var currentDiv = 0;
+    var $div = $('#content-container-1');
 
+    
     // Add a click event listener to the scroll button
     $('#scrollButton').click(function () {
-        // Scroll down to the second div if not already at the last div
-        if (currentDiv < $divs.length - 1) {
-            currentDiv++;
-            var targetTop = $divs.eq(currentDiv).offset().top;
-            $('html, body').stop().animate({
-                scrollTop: targetTop
-            }, 600);
-        }
+        // Scroll down to the second div
+        var targetTop = $div.offset().top;
+        $('html, body').stop().animate({
+            scrollTop: targetTop
+        }, 600);
+
     });
 });
